7302e5867323cf542cccfb9ac852a9d4ce11d0bf
bry4nG4rrix <bryanmfb4@gmail.com>
2024-11-05 19:13:28 +0300

    last

---
+++ assets/js/bg.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".typed",{
+    strings:["Developer fullstack !","Ui/UX designer !","freelancer !","tsy pd",],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true,
+    backDelay:1500
+  })
